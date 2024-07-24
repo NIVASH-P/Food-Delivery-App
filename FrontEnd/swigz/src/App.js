4cd10e7cf365c541692d57b03235b2a793e06db9
Parthiban-P-dev <727722euit125@skcet.ac.in>
2024-07-24 23:37:32 +0530

    menu,dishes completed

--- FrontEnd/swigz/src/App.js
+++ FrontEnd/swigz/src/App.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import './App.css';
+import { Exploremenu } from './components/Exploremenu/Exploremenu';
 import NavBar from './components/NavBar/NavBar';
+import { Fooddisplay } from './components/Fooddisplay/Fooddisplay';
 
 function App() {
+
+  const [category,setCategory]=useState("All");
   return (
     <div className="App">
         <NavBar />
+         <Exploremenu category={category} setCategory={setCategory}/>
+         <Fooddisplay category={category}/>
     </div>
   );
 }
