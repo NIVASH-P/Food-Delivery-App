4f146c25ca669aebdab5410684842d9696760496
prajanravichandra <727722euit129@skcet.ac.in>
2024-07-25 12:27:27 +0530

    home page completed

--- FrontEnd/swigz/src/components/Header/Header.js
+++ FrontEnd/swigz/src/components/Header/Header.js
@@ -5,7 +5,7 @@ export default function
   return (
     <div className='header'>
         <div className='header-contents'>
-        <h2>get your customizable food diet for better health</h2>
+        <h2>get your customizable food diet for better health.</h2>
         <p>we are providing customizable diet food on subscription basis according to your health check</p>
         <button>subscribe</button>
 
