9e766a26a1a84b199bb79988492e7d2b5369f239
NIVASH <124855626+NIVASH-P@users.noreply.github.com>
2024-08-05 15:09:53 +0530

    Update App.js

--- FrontEnd/swigz/src/App.js
+++ FrontEnd/swigz/src/App.js
@@ -1,16 +1,28 @@
+import { useState } from 'react';
+import { Exploremenu } from './components/Exploremenu/Exploremenu';
+import NavBar from './components/NavBar/NavBar';
+import { Fooddisplay } from './components/Fooddisplay/Fooddisplay';
 // src/App.js
 import React from 'react';
 
+import './App.css';
+import { PlaceOrder } from './components/pages/PlaceOrder/PlaceOrder';
+function App() {
 import Header from './components/Header/Header';
 
+  const [category,setCategory]=useState("All");
 function App() {
   return (
-   
+    <div className="App">
+        <PlaceOrder/>
+        <NavBar />
+         <Exploremenu category={category} setCategory={setCategory}/>
+         <Fooddisplay category={category}/>
+    </div>
+
       <div className="App">
           <Header/>
       </div>
-    
+
   );
 }
-
-export default App;
