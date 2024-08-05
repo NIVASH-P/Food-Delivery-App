61d001b197a524417ae7556b0aeec1ec96be0c4c
NIVASH-P <nivashpns666@gmail.com>
2024-08-05 15:28:20 +0530

    modified according to pull request

--- FrontEnd/swigz/src/components/Exploremenu/context.jsx
+++ FrontEnd/swigz/src/components/Exploremenu/context.jsx
@@ -1,39 +1,57 @@
 import { createContext, useEffect, useState } from "react";
 import { food_list } from "../../Assests/assets";
 
-export const StoreContext = createContext(null);
+export const StoreContext=createContext(null)
 
-const StoreContextProvider = (props) => {
-  const [cardItems, setCardItems] = useState({});
+const StoreContextProvider =(props)=>{
 
-  const addToCard = (itemId) => {
-    if (!cardItems[itemId]) {
-      setCardItems((prev) => ({ ...prev, [itemId]: 1 }));
-    } else {
-      setCardItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    const [cardItems , setCardItems] = useState({});
+
+    const addToCard =(itemId)=>{
+             
+        if(!cardItems[itemId]){
+            setCardItems((prev)=>({...prev,[itemId]:1}))
+        }
+        else{
+            setCardItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+
+        }
     }
-  };
 
-  const removeFromCard = (itemId) => {
-    // console.log("Hello");
-    setCardItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
-  };
+    const removeFromCard=(itemId)=>{
+        setCardItems((prev)=>({...prev,[itemId]:prev[itemId]-1
+        }))
+    }
 
-  useEffect(() => {
-    // console.log("from context", cardItem-);
-  }, [cardItems]);
+   const getTotalcard =()=>{
+    let total=0;
+    for(const item in cardItems)
+    {
+        if(cardItems[item]>0){
 
-  const contextValue = {
-    food_list,
-    cardItems,
-    setCardItems,
-    addToCard,
-    removeFromCard,
-  };
-  return (
-    <StoreContext.Provider value={contextValue}>
-      {props.children}
-    </StoreContext.Provider>
-  );
-};
-export default StoreContextProvider;
+        let iteminfo=food_list.find((product)=>product._id==item);
+        total+=iteminfo.price* cardItems[item]
+
+        }
+    }
+    return total;
+   }
+
+    const contextValue={
+          
+        food_list,
+        cardItems,
+        setCardItems,
+        addToCard,
+        removeFromCard,
+        getTotalcard
+
+
+    }
+    return(
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
+export default StoreContextProvider
